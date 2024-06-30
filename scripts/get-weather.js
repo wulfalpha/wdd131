f6f856163dbb3114836d7cc9780ba22dcf17b5e6
Andrew <drace.davidson@gmail.com>
2024-06-29 20:07:31 -0600

    wettr

--- scripts/get-weather.js
+++ scripts/get-weather.js
@@ -15,15 +15,29 @@ const updateHeroImage = () => {
     const smallSrc = 'https://source.unsplash.com/random/400x300/?united-kingdom';
     const largeSrc = 'https://source.unsplash.com/random/800x600/?united-kingdom';
     const sources = heroImageElement.querySelectorAll('source');
-    sources[0].srcset = smallSrc;
-    sources[1].srcset = largeSrc;
-    heroImageElement.querySelector('img').src = largeSrc;
+    if (sources.length > 1) {
+        sources[0].srcset = smallSrc;
+        sources[1].srcset = largeSrc;
+    }
+    const img = heroImageElement.querySelector('img');
+    if (img) {
+        img.src = largeSrc;
+    }
 };
 
 const updateWeatherData = async () => {
     try {
         const response = await fetch(WEATHER_API_URL);
         const data = await response.json();
+        let weatherIco = document.getElementById('weather_ico');
+
+        // Convert the weather description to a format matching our file names
+        let description = data.weather[0].description.toLowerCase().replace(/ /g, '');
+
+        // Correctly form the path to the SVG file in the 'images' folder
+        // Ensure it correctly points to rainy, sunny, hail, windy, stormy, snow, or cloudy .svg files
+        weatherIco.src = `images/${description}.svg`;
+
         temperatureElement.textContent = `<strong>Temperature:</strong> ${data.main.temp}°C`;
         conditionsElement.textContent = `<strong>Conditions:</strong> ${data.weather[0].description}`;
         windElement.textContent = `<strong>Wind:</strong> ${data.wind.speed} km/h`;
