bd94ee3540f7cb21c6eb1ed8b7a427326a67fe20
Andrew <drace.davidson@gmail.com>
2024-07-07 00:06:38 -0600

    filtered

---
+++ scripts/filtered-temples.js
@@ -0,0 +1,160 @@
+defer = true
+const hamButton = document.querySelector('#menu');
+const navigation = document.querySelector('.navigation');
+
+hamButton.addEventListener('click', () => {
+    navigation.classList.toggle('open');
+    hamButton.classList.toggle('open');
+});
+
+!function(d,s,id){var js,fjs=d.getElementsByTagName(s)[0];if(!d.getElementById(id)){js=d.createElement(s);js.id=id;js.src='https://weatherwidget.io/js/widget.min.js';fjs.parentNode.insertBefore(js,fjs);}}(document,'script','weatherwidget-io-js');
+
+
+document.addEventListener('DOMContentLoaded', () => {
+    const rightsSection = document.querySelector("#rights");
+    const paragraphs = rightsSection.querySelectorAll("p");
+    const now = new Date();
+    const dateString = now.toLocaleString();
+    paragraphs[1].textContent = `Date and Time: ${dateString}`;
+    document.querySelector("[data-modified-date]").textContent = document.lastModified;
+
+    let pTag = document.querySelector('.card p'); // More precise selection of p tag
+    if(pTag && visits) {
+        pTag.innerHTML = pTag.innerHTML.replace('[visits]', visits);
+    }
+});
+
+const temples = [
+    {
+        templeName: "Aba Nigeria",
+        location: "Aba, Nigeria",
+        dedicated: "2005, August, 7",
+        area: 11500,
+        imageUrl:
+            "https://content.churchofjesuschrist.org/templesldsorg/bc/Temples/photo-galleries/aba-nigeria/400x250/aba-nigeria-temple-lds-273999-wallpaper.jpg"
+    },
+    {
+        templeName: "Manti Utah",
+        location: "Manti, Utah, United States",
+        dedicated: "1888, May, 21",
+        area: 74792,
+        imageUrl:
+            "https://content.churchofjesuschrist.org/templesldsorg/bc/Temples/photo-galleries/manti-utah/400x250/manti-temple-768192-wallpaper.jpg"
+    },
+    {
+        templeName: "Payson Utah",
+        location: "Payson, Utah, United States",
+        dedicated: "2015, June, 7",
+        area: 96630,
+        imageUrl:
+            "https://content.churchofjesuschrist.org/templesldsorg/bc/Temples/photo-galleries/payson-utah/400x225/payson-utah-temple-exterior-1416671-wallpaper.jpg"
+    },
+    {
+        templeName: "Yigo Guam",
+        location: "Yigo, Guam",
+        dedicated: "2020, May, 2",
+        area: 6861,
+        imageUrl:
+            "https://content.churchofjesuschrist.org/templesldsorg/bc/Temples/photo-galleries/yigo-guam/400x250/yigo_guam_temple_2.jpg"
+    },
+    {
+        templeName: "Washington D.C.",
+        location: "Kensington, Maryland, United States",
+        dedicated: "1974, November, 19",
+        area: 156558,
+        imageUrl:
+            "https://content.churchofjesuschrist.org/templesldsorg/bc/Temples/photo-galleries/washington-dc/400x250/washington_dc_temple-exterior-2.jpeg"
+    },
+    {
+        templeName: "Lima Perú",
+        location: "Lima, Perú",
+        dedicated: "1986, January, 10",
+        area: 9600,
+        imageUrl:
+            "https://content.churchofjesuschrist.org/templesldsorg/bc/Temples/photo-galleries/lima-peru/400x250/lima-peru-temple-evening-1075606-wallpaper.jpg"
+    },
+    {
+        templeName: "Mexico City Mexico",
+        location: "Mexico City, Mexico",
+        dedicated: "1983, December, 2",
+        area: 116642,
+        imageUrl:
+            "https://content.churchofjesuschrist.org/templesldsorg/bc/Temples/photo-galleries/mexico-city-mexico/400x250/mexico-city-temple-exterior-1518361-wallpaper.jpg"
+    },
+    {
+        templeName: "American Fork Temple",
+        location: "American Fork, Utah, United States",
+        dedicated: "1996, October, 13",
+        area: 107240,
+        imageUrl:
+            "https://content.churchofjesuschrist.org/templesldsorg/bc/Temples/photo-galleries/american-fork-utah/400x250/american-fork-utah-temple-exterior-1518361-wallpaper.jpg"
+    },
+    {
+        templeName: "Provo City Center Temple",
+        location: "Provo, Utah, United States",
+        dedicated: "2016, March, 20",
+        area: 85084,
+        imageUrl:
+            "https://content.churchofjesuschrist.org/templesldsorg/bc/Temples/photo-galleries/provo-city-center/400x250/provo_city_center_temple-exterior-2.jpeg"
+    },
+    {
+        templeName: "Anchorage Alaska Temple",
+        location: "Anchorage, Alaska, United States",
+        dedicated: "1999, January, 9",
+        area: 11937,
+        imageUrl:
+            "https://content.churchofjesuschrist.org/templesldsorg/bc/Temples/photo-galleries/washington-dc/400x250/washington_dc_temple-exterior-2.jpeg"
+    }
+];
+
+const input = document.querySelector('#favchap')
+const button = document.querySelector('#submit')
+console.log(button);
+const list = document.querySelector('#list') // Changed this line
+let chaptersArray = getChapterList() || [];
+
+chaptersArray.forEach(chapter => {
+    displayList(chapter);
+});
+
+function displayList(item) {
+    let li = document.createElement('li');
+    let deletebutton = document.createElement('button');
+    li.textContent = item; // note the use of the displayList parameter 'item'
+    deletebutton.textContent = '❌';
+    deletebutton.classList.add('delete'); // this references the CSS rule .delete{width:fit-content;} to size the delete button
+    li.append(deletebutton);
+    list.append(li);
+    deletebutton.addEventListener('click', function () {
+        list.removeChild(li);
+        deleteChapter(li.textContent); // note this new function that is needed to remove the chapter from the array and localStorage.
+        input.focus(); // set the focus back to the input
+    });
+    console.log('I like to copy code instead of typing it out myself and trying to understand it.');
+}
+function setChapterList() {
+    try {
+        const serializedData = JSON.stringify(chaptersArray);
+        localStorage.setItem('myFavBOMList', serializedData);
+    } catch (e) {
+        console.log('Error saving to localStorage', e);
+    }
+}
+function getChapterList() {
+    const chapters = localStorage.getItem('myFavBOMList');
+    return chapters ? JSON.parse(chapters) : [];
+}
+function deleteChapter(chapter) {
+    chapter = chapter.slice(0, chapter.length - 1);
+    chaptersArray = chaptersArray.filter(item => item !== chapter);
+    setChapterList();
+}
+button.addEventListener('click', () => {
+    if (input.value !== '') {  // make sure the input is not empty
+        displayList(input.value); // call the function that outputs the submitted chapter
+        chaptersArray.push(input.value);  // add the chapter to the array
+        setChapterList(); // update the localStorage with the new array
+        input.value = ''; // clear the input
+        input.focus(); // set the focus back to the input
+    }
+});
