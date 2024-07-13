f6c6ecc81b8912b08d3dee4004f8baa0b8f3bdb7
Andrew <drace.davidson@gmail.com>
2024-07-13 16:33:30 -0600

    Kitttz

---
+++ project/scripts/siteplan.js
@@ -0,0 +1,77 @@
+defer = true
+const hamButton = document.querySelector('#menu');
+const navigation = document.querySelector('.navigation');
+
+hamButton.addEventListener('click', () => {
+    navigation.classList.toggle('open');
+    hamButton.classList.toggle('open');
+});
+
+!function(d,s,id){var js,fjs=d.getElementsByTagName(s)[0];if(!d.getElementById(id)){js=d.createElement(s);js.id=id;js.src='https://weatherwidget.io/js/widget.min.js';fjs.parentNode.insertBefore(js,fjs);}}(document,'script','weatherwidget-io-js');
+
+
+document.addEventListener('DOMContentLoaded', () => {
+    const rightsSection = document.querySelector("#rights");
+    const paragraphs = rightsSection.querySelectorAll("p");
+    const now = new Date();
+    const dateString = now.toLocaleString();
+    paragraphs[1].textContent = `Date and Time: ${dateString}`;
+    document.querySelector("[data-modified-date]").textContent = document.lastModified;
+
+    let pTag = document.querySelector('.card p'); // More precise selection of p tag
+    if(pTag && visits) {
+        pTag.innerHTML = pTag.innerHTML.replace('[visits]', visits);
+    }
+});
+
+const input = document.querySelector('#favchap')
+const button = document.querySelector('#submit')
+console.log(button);
+const list = document.querySelector('#list') // Changed this line
+let chaptersArray = getChapterList() || [];
+
+chaptersArray.forEach(chapter => {
+    displayList(chapter);
+});
+
+function displayList(item) {
+    let li = document.createElement('li');
+    let deletebutton = document.createElement('button');
+    li.textContent = item; // note the use of the displayList parameter 'item'
+    deletebutton.textContent = '❌';
+    deletebutton.classList.add('delete'); // this references the CSS rule .delete{width:fit-content;} to size the delete button
+    li.append(deletebutton);
+    list.append(li);
+    deletebutton.addEventListener('click', function () {
+        list.removeChild(li);
+        deleteChapter(li.textContent); // note this new function that is needed to remove the chapter from the array and localStorage.
+        input.focus(); // set the focus back to the input
+    });
+    console.log('I like to copy code instead of typing it out myself and trying to understand it.');
+}
+function setChapterList() {
+    try {
+        const serializedData = JSON.stringify(chaptersArray);
+        localStorage.setItem('myFavBOMList', serializedData);
+    } catch (e) {
+        console.log('Error saving to localStorage', e);
+    }
+}
+function getChapterList() {
+    const chapters = localStorage.getItem('myFavBOMList');
+    return chapters ? JSON.parse(chapters) : [];
+}
+function deleteChapter(chapter) {
+    chapter = chapter.slice(0, chapter.length - 1);
+    chaptersArray = chaptersArray.filter(item => item !== chapter);
+    setChapterList();
+}
+button.addEventListener('click', () => {
+    if (input.value !== '') {  // make sure the input is not empty
+        displayList(input.value); // call the function that outputs the submitted chapter
+        chaptersArray.push(input.value);  // add the chapter to the array
+        setChapterList(); // update the localStorage with the new array
+        input.value = ''; // clear the input
+        input.focus(); // set the focus back to the input
+    }
+});
